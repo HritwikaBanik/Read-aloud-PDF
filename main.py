e08fba8a909f91288ad2dcaa65e326d4ce52bb1c
HRITWIKA BANIK <98072016+Hritwika-banik@users.noreply.github.com>
2022-01-20 10:18:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+ #Importing Libraries
 import pyttsx3
 import PyPDF2
 ebook = open('AI.pdf', 'rb')
@@ -5,6 +6,11 @@ pdfreader = PyPDF2.PdfFileReader(ebook)
 pages = pdfreader.numPages
 print(pages)
 speaker = pyttsx3.init()
+
+#for a female voice : 0 for male,1 for female
+voices = speaker.getProperty("voices")
+speaker.setProperty('voice', voices[1].id)
+
 for num in range(6, pages):
     page = pdfreader.getPage(6)
     text = page.extractText()
